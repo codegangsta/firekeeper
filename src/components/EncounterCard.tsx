f2f73bbd7dcbaebc5e516122e9c7e8f17c08c9c6
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-03-06 09:38:39 -0800

    Starting to render cards

---
+++ src/components/EncounterCard.tsx
@@ -0,0 +1,83 @@
+interface TileItemsProps {
+  id: number;
+  top: number;
+  left: number;
+}
+
+function TileItems(props: TileItemsProps) {
+  const { id, top, left } = props;
+  return (
+    <div
+      className="absolute"
+      style={{ width: 256, height: 242, top: top, left: left }}
+    >
+      <img src="/images/tile-items.png" />
+    </div>
+  );
+}
+
+export default function EncounterCard() {
+  return (
+    <div
+      className="rounded-3xl overflow-clip relative"
+      style={{ width: 807, height: 1397 }}
+    >
+      <img src="/images/encounter-card.jpg" />
+      {/* Title */}
+      <span
+        className="absolute text-white text-6xl flex text-center items-center justify-center font-dark-souls"
+        style={{ top: 35, left: 188, width: 430, height: 120 }}
+      >
+        The Locked Grave
+      </span>
+      {/* Flavor */}
+      <span
+        className="absolute text-black text-xl flex text-center items-center justify-center inset-x-0 mx-24 italic"
+        style={{ top: 178, height: 58 }}
+      >
+        The passage ahead ends in a stout door, sealed by an ancient lock, but
+        where there is a lock there must also be a key...
+      </span>
+      {/* Objective */}
+      <div
+        className="absolute text-black/70 text-2xl flex flex-col leading-7 inset-x-0"
+        style={{ top: 270, height: 58, left: 42, right: 42 }}
+      >
+        <span className="font-dark-souls font-semibold tracking-wider  text-[26px] leading-8">
+          Objective:
+        </span>
+        <span className="text-black">Open all chests.</span>
+      </div>
+      {/* Rewards */}
+      <div
+        className="absolute text-black/70 text-2xl flex flex-col leading-6 inset-x-0 "
+        style={{ top: 370, height: 250, left: 42, width: 220 }}
+      >
+        <span className="font-dark-souls font-semibold tracking-wider text-[26px] leading-8">
+          Rewards:
+        </span>
+        <span className="text-black font-bold">Souls:</span>
+        <span>2x Souls per enemy killed</span>
+        <span className="text-black font-bold">Draw:</span>
+        <span>1x Event</span>
+        <span className="text-black font-bold">Trial:</span>
+        <span>Dragon Scale</span>
+        <span className="text-black font-bold">Refresh:</span>
+        <span>Estus Flask</span>
+      </div>
+      {/* Special Rules */}
+      <div
+        className="absolute text-black/70 font-semibold text-2xl flex flex-col leading-7 inset-x-0"
+        style={{ top: 370, height: 250, left: 285, width: 220 }}
+      >
+        <span className="font-dark-souls tracking-wider text-[26px] leading-8">
+          Special Rules:
+        </span>
+        <span className="text-black">Open all chests.</span>
+      </div>
+      <TileItems id={1} top={650} left={540} />)
+      <TileItems id={2} top={895} left={540} />)
+      <TileItems id={3} top={1140} left={540} />)
+    </div>
+  );
+}
