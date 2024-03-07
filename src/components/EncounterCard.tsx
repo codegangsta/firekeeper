5f95a69b2158c3acd38c04cd26cf9192d76c0a2d
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-03-06 20:00:26 -0800

    More tweaking styling

--- src/components/EncounterCard.tsx
+++ src/components/EncounterCard.tsx
@@ -269,117 +269,136 @@ function Tile(props: PropsWithChildren<TileProps>) {
   );
 }
 
-export default function EncounterCard() {
+interface Props {
+  scale?: number;
+}
+
+export default function EncounterCard({ scale = 1 }: Props = {}) {
   return (
-    <div className="w-[807px] h-[1397px] rounded-xl overflow-clip relative">
-      <img src="/images/encounter-card.jpg" />
-      <img
-        className="absolute"
-        src="/images/difficulty-3.png"
-        style={{ width: 128, height: 128, top: 29, right: 19 }}
-      />
+    <div
+      className="rounded-xl border border-zinc-700 overflow-clip"
+      style={{
+        width: 807 * scale,
+        height: 1397 * scale,
+      }}
+    >
       <div
-        className="absolute rounded-full bg-black"
-        style={{ width: 111, height: 111, top: 39, left: 28 }}
-      ></div>
-      {/* Title */}
-      <span
-        className="absolute text-white text-6xl flex text-center items-center justify-center font-dark-souls"
-        style={{ top: 35, left: 188, width: 430, height: 120 }}
+        className="relative origin-top-left"
+        style={{
+          transform: `scale(${scale})`,
+          width: 807,
+          height: 1397,
+        }}
       >
-        The Locked Grave
-      </span>
-      {/* Flavor */}
-      <span
-        className="absolute text-black text-xl flex text-center items-center justify-center inset-x-0 mx-24 italic"
-        style={{ top: 178, height: 58 }}
-      >
-        The passage ahead ends in a stout door, sealed by an ancient lock, but
-        where there is a lock there must also be a key...
-      </span>
-      {/* Objective */}
-      <div
-        className="absolute text-black/70 text-2xl flex flex-col leading-7 inset-x-0"
-        style={{ top: 270, height: 58, left: 42, right: 42 }}
-      >
-        <span className="font-dark-souls font-semibold tracking-wider  text-[26px] leading-8">
-          Objective:
+        <img src="/images/encounter-card.jpg" />
+        <img
+          className="absolute"
+          src="/images/difficulty-3.png"
+          style={{ width: 128, height: 128, top: 29, right: 19 }}
+        />
+        <div
+          className="absolute rounded-full bg-black"
+          style={{ width: 111, height: 111, top: 39, left: 28 }}
+        ></div>
+        {/* Title */}
+        <span
+          className="absolute text-white text-6xl flex text-center items-center justify-center font-dark-souls"
+          style={{ top: 35, left: 188, width: 430, height: 120 }}
+        >
+          The Locked Grave
         </span>
-        <span className="text-black">Open all chests.</span>
-      </div>
-      {/* Rewards */}
-      <div
-        className="absolute text-black/70 text-2xl flex flex-col leading-6 inset-x-0 "
-        style={{ top: 370, height: 250, left: 42, width: 220 }}
-      >
-        <span className="font-dark-souls font-semibold tracking-wider text-[26px] leading-8">
-          Rewards:
+        {/* Flavor */}
+        <span
+          className="absolute text-black text-xl flex text-center items-center justify-center inset-x-0 mx-24 italic"
+          style={{ top: 178, height: 58 }}
+        >
+          The passage ahead ends in a stout door, sealed by an ancient lock, but
+          where there is a lock there must also be a key...
         </span>
-        <span className="text-black font-bold">Souls:</span>
-        <span>2x Souls per enemy killed</span>
-        <span className="text-black font-bold">Draw:</span>
-        <span>1x Event</span>
-        <span className="text-black font-bold">Trial:</span>
-        <span>Dragon Scale</span>
-        <span className="text-black font-bold">Refresh:</span>
-        <span>Estus Flask</span>
-      </div>
-      {/* Special Rules */}
-      <div
-        className="absolute text-black/70 font-semibold text-2xl flex flex-col leading-7 inset-x-0"
-        style={{ top: 370, height: 250, left: 285, width: 475 }}
-      >
-        <span className="font-dark-souls tracking-wider text-[26px] leading-8">
-          Special Rules:
-        </span>
-        <div className="flex flex-col gap-3 leading-6">
-          <span className="text-black italic">
-            Trial: Kill the Skeleton Beast
+        {/* Objective */}
+        <div
+          className="absolute text-black/70 text-2xl flex flex-col leading-7 inset-x-0"
+          style={{ top: 270, height: 58, left: 42, right: 42 }}
+        >
+          <span className="font-dark-souls font-semibold tracking-wider  text-[26px] leading-8">
+            Objective:
           </span>
-          <span className="font-medium">
-            If the lever is activated, spawn a Skeleton Beast on tile three, on
-            the closest enemy spawn node to the character.
-          </span>
+          <span className="text-black">Open all chests.</span>
         </div>
-      </div>
-      {/* Tiles Items */}
-      <TileItems
-        id={1}
-        top={640}
-        left={540}
-        node1={["/icons/giant-skeleton-archer.png"]}
-        node2={["/icons/skeleton-soldier.png"]}
-        node3="/icons/tombstone.png"
-      />
-      <TileItems
-        id={2}
-        top={890}
-        left={540}
-        trap
-        node1={["/icons/giant-skeleton-soldier.png"]}
-        node2={["/icons/necromancer.png"]}
-        node3="/icons/lever.png"
-        node4="/icons/barrel.png"
-      />
-      )
-      <TileItems
-        id={3}
-        top={1140}
-        left={540}
-        node1={["/icons/giant-skeleton-archer.png"]}
-        node2={["/icons/skeleton-archer.png"]}
-        node3="/icons/treasure-chest.png"
-        node4="/icons/treasure-chest.png"
-      />
-      <div
-        className="absolute flex flex-col items-center justify-center"
-        style={{ width: 493, height: 710, top: 663, left: 30 }}
-      >
-        <Tile id={1} exit="top" entrance="bottom">
-          <Tile id={2} exit="left">
-            <Tile id={3} exit="top" />
+        {/* Rewards */}
+        <div
+          className="absolute text-black/70 text-2xl flex flex-col leading-6 inset-x-0 "
+          style={{ top: 370, height: 250, left: 42, width: 220 }}
+        >
+          <span className="font-dark-souls font-semibold tracking-wider text-[26px] leading-8">
+            Rewards:
+          </span>
+          <span className="text-black font-bold">Souls:</span>
+          <span>2x Souls per enemy killed</span>
+          <span className="text-black font-bold">Draw:</span>
+          <span>1x Event</span>
+          <span className="text-black font-bold">Trial:</span>
+          <span>Dragon Scale</span>
+          <span className="text-black font-bold">Refresh:</span>
+          <span>Estus Flask</span>
+        </div>
+        {/* Special Rules */}
+        <div
+          className="absolute text-black/70 font-semibold text-2xl flex flex-col leading-7 inset-x-0"
+          style={{ top: 370, height: 250, left: 285, width: 475 }}
+        >
+          <span className="font-dark-souls tracking-wider text-[26px] leading-8">
+            Special Rules:
+          </span>
+          <div className="flex flex-col gap-3 leading-6">
+            <span className="text-black italic">
+              Trial: Kill the Skeleton Beast
+            </span>
+            <span className="font-medium">
+              If the lever is activated, spawn a Skeleton Beast on tile three,
+              on the closest enemy spawn node to the character.
+            </span>
+          </div>
+        </div>
+        {/* Tiles Items */}
+        <TileItems
+          id={1}
+          top={640}
+          left={540}
+          node1={["/icons/giant-skeleton-archer.png"]}
+          node2={["/icons/skeleton-soldier.png"]}
+          node3="/icons/tombstone.png"
+        />
+        <TileItems
+          id={2}
+          top={890}
+          left={540}
+          trap
+          node1={["/icons/giant-skeleton-soldier.png"]}
+          node2={["/icons/necromancer.png"]}
+          node3="/icons/lever.png"
+          node4="/icons/barrel.png"
+        />
+        )
+        <TileItems
+          id={3}
+          top={1140}
+          left={540}
+          node1={["/icons/giant-skeleton-archer.png"]}
+          node2={["/icons/skeleton-archer.png"]}
+          node3="/icons/treasure-chest.png"
+          node4="/icons/treasure-chest.png"
+        />
+        <div
+          className="absolute flex flex-col items-center justify-center"
+          style={{ width: 493, height: 710, top: 663, left: 30 }}
+        >
+          <Tile id={1} exit="top" entrance="bottom">
+            <Tile id={2} exit="left">
+              <Tile id={3} exit="top" />
+            </Tile>
           </Tile>
-        </Tile>
+        </div>
       </div>
     </div>
   );
