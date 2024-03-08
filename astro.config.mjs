cf60f92049d3f06a0343fbb167ba28e10669e41a
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-03-07 18:53:20 -0800

    Install vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
-  integrations: [tailwind(), react()],
 });
